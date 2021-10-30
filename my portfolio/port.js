80e617a5531b3282545f9ede4b6662f7b0802b50
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2021-10-30 12:02:00 +0500

    My portfolio
    https://hmk1.netlify.app/?fbclid=IwAR2Z_0Xmy3_WU_Kh2ZNibH4BZ3iWiDzptZfwenlyHUd1HHyKgokRII_u4iI

---
+++ my portfolio/port.js
@@ -0,0 +1,10 @@
+const burger = document.querySelector('.burger')
+// const nav = document.querySelector('.main-list')
+
+// burger.addEventListener('click',() => {
+// 	nav.classList.toggle('nav-active')
+// })
+let btn =document.querySelector('.submit')
+btn.addEventListener('click', () => {
+	alert('message not send')
+})
